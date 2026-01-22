ede37f66e708431e65e909271cdb8ed11d501bae
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-01-21 19:58:12 -0800

    user prompt for first and second numbers

--- COMP003A.LectureActivity2/Program.cs
+++ COMP003A.LectureActivity2/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace COMP003A.LectureActivity2
+﻿using System.Collections;
+
+namespace COMP003A.LectureActivity2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, user!");
+
+            Console.Write("Enter the first number: ");
+            string firstNumber = Console.ReadLine();
+
+            Console.Write("Enter the second number: ");
+            string secondNumber = Console.ReadLine();
+
+
+            
         }
     }
 }
